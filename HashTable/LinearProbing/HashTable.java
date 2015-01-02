cc082f702e028fb6fb16c21ca4179cbc29f28ff6
ben med <benaich.med@gmail.com>
2015-01-02 22:15:29 +0000

    implement HashTableInterface

--- HashTable/LinearProbing/HashTable.java
+++ HashTable/LinearProbing/HashTable.java
@@ -1,13 +1,10 @@
 package HashTable.LinearProbing;
 
+import HashTable.HashTableInterface;
 import java.util.Random;
 import java.util.function.Function;
 
-public class HashTable<K, V> {
-
-    public static final int INITIAL_TABLE_SIZE = 10;
-    public static final double LOAD_FACTOR = 0.5;
-    public static final int BIGNUMBER = 3345;
+public class HashTable<K, V> implements HashTableInterface<K, V> {
 
     private Node[] array;
     private Function<K, Integer> foh;
@@ -26,14 +23,12 @@ public class HashTable<K, V> {
         foh = f;
     }
 
-    public float loadFactor() {
-        return size / (float) tableSize;
-    }
-
+    @Override
     public int indexOf(K key) {
         return foh.apply(key);
     }
 
+    @Override
     public void put(K key, V val) {
         if (loadFactor() > LOAD_FACTOR) {
             resize();
@@ -52,6 +47,7 @@ public class HashTable<K, V> {
         array[i] = new Node(key, val);
     }
 
+    @Override
     public V get(K key) {
         int i = indexOf(key);
         while (array[i] != null && !array[i].getKey().equals(key)) {
@@ -60,19 +56,30 @@ public class HashTable<K, V> {
         return (array[i] != null) ? (V) array[i].getValue() : null;
     }
 
+    @Override
     public void delete(K key) {
         int i = indexOf(key);
         while (array[i] != null && !array[i].getKey().equals(key)) {
             i = (i + 1) % array.length;
         }
         array[i] = null;
+        size--;
     }
 
+    @Override
+    public void clear() {
+        array = new Node[tableSize];
+        size = 0;
+        numCollisions = 0;
+    }
+
+    @Override
     public void resize() {
-        numResizes++;
         Node[] oldArray = array;
-        array = new Node[tableSize * 2];
         tableSize = tableSize * 2;
+        numResizes++;
+        clear();
+
         for (int i = 0; i < oldArray.length; i++) {
             Node<K, V> node = oldArray[i];
             if (node != null) {
@@ -81,103 +88,79 @@ public class HashTable<K, V> {
         }
     }
 
+    @Override
     public int getSize() {
         return size;
     }
 
+    @Override
+    public int getTableSize() {
+        return tableSize;
+    }
+
+    @Override
     public int getNumCollisions() {
         return numCollisions;
     }
 
+    @Override
     public int getNumResizes() {
         return numResizes;
     }
 
-    private Function<K, Integer> getHashingFunction() {
-
-        Random r = new Random();
-        int a, b, p;
-        a = r.nextInt();
-        b = r.nextInt();
-        p = nextPrime(BIGNUMBER);
-
-        return (x) -> Math.abs(((a * x.hashCode() + b * x.hashCode()) * p) % tableSize);
-    }
-
+    @Override
     public void print() {
         for (int i = 0; i < array.length; i++) {
-
+            System.out.print(i + " -> ");
             if (array[i] != null) {
-                System.out.print(i + " -> " + array[i] + indexOf((K) array[i].getKey()));
-            } else {
-                System.out.print(i + " -> x");
+                System.out.print(array[i] + " " + indexOf((K) array[i].getKey()));
             }
             System.out.println();
         }
     }
 
-    /* helpers */
-    private static int nextPrime(int a) {
-        while (!isPrime(a)) {
-            a++;
-        }
-        return a;
-    }
-
-    private static boolean isPrime(int a) {
-        if (a == 2) {
-            return true;
-        } else if (a % 2 == 0) {
-            return false;
-        } else {
-            int i = 3, end = (int) Math.sqrt(a + 0.0);
-            while (a % i != 0 && i <= end) {
-                i = i + 2;
+    @Override
+    public void printGraph() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(i + " -> ");
+            if (array[i] != null) {
+                System.out.print(indexOf((K) array[i].getKey()));
             }
-            return i > end;
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        Function<String, Integer> modulo = x -> Math.abs(x.hashCode()) % INITIAL_TABLE_SIZE;
 
-        HashTable<String, String> ht = new HashTable<>(modulo);
+        HashTable<String, String> ht = new HashTable<>();
         ht.put("banana", "yellow");
         ht.put("apple", "green");
         ht.put("android", "green");
         ht.put("cat", "white");
         ht.put("body", "black");
         ht.put("glass", "red");
+        ht.put("jessy", "pinkman");
+        ht.put("walter", "white");
+        ht.put("arya", "startk");
+        ht.put("dexter", "red");
 
         System.out.println("----------- Print -----------");
         ht.print();
 
         System.out.println("----------- Get -----------");
-        String deletedKey = "body";
+        String deletedKey = "apple";
         System.out.println(deletedKey + " : i -> " + ht.indexOf(deletedKey) + " -> " + ht.get(deletedKey));
 
         System.out.println("----------- Delete -----------");
         ht.delete(deletedKey);
-        System.out.println("----------- Print -----------");
-        ht.print();
+        ht.printGraph();
+        ht.printStatus();
 
-        /*HashTable<String, String> table = new HashTable<>(modulo);
-
-         for (int i = 0; i < 100000; i++) {
-         table.put(randomString(), randomString());
-         }
-
-         System.out.println(table.getNumCollisions());
-         System.out.println(table.getNumResizes());*/
-    }
-
-    public static String randomString() {
-        Random r = new Random(); // perhaps make it a class variable so you don't make a new one every time
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-            char c = (char) (r.nextInt((int) (Character.MAX_VALUE)));
-            sb.append(c);
+        // Testing
+        HashTable<String, String> table = new HashTable<>();
+        for (int i = 0; i < 100000; i++) {
+            table.put(HashTableInterface.randomString(), HashTableInterface.randomString());
         }
-        return sb.toString();
+        table.printStatus();
     }
 }
--- HashTable/SeparateChaining/HashTable.java
+++ HashTable/SeparateChaining/HashTable.java
@@ -1,39 +1,33 @@
 package HashTable.SeparateChaining;
 
-import java.util.Random;
+import HashTable.HashTableInterface;
 import java.util.function.Function;
 
-public class HashTable<K, V> {
-
-    public static final int INITIAL_TABLE_SIZE = 10;
-    public static final double LOAD_FACTOR = 0.5;
-    public static final int BIGNUMBER = 3345;
+public class HashTable<K, V> implements HashTableInterface<K, V> {
 
     private Node[] array;
     private Function<K, Integer> foh;
     private int tableSize = INITIAL_TABLE_SIZE;
-    private int size;
-    private static int numCollisions = 0;
-    private static int numResizes = 0;
+    private int size = 0;
+    private int numCollisions = 0;
+    private int numResizes = 0;
 
     public HashTable() {
         array = new Node[tableSize];
-        CreateHashingFunction();
+        foh = getHashingFunction();
     }
 
     public HashTable(Function<K, Integer> f) {
-        array = new Node[INITIAL_TABLE_SIZE];
+        array = new Node[tableSize];
         foh = f;
     }
 
-    public float loadFactor() {
-        return size / (float) tableSize;
-    }
-
+    @Override
     public int indexOf(K key) {
         return foh.apply(key);
     }
 
+    @Override
     public void put(K key, V val) {
         int index = indexOf(key);
 
@@ -42,18 +36,21 @@ public class HashTable<K, V> {
         }
 
         Node<K, V> node = array[index];
-        while (null != node) {
+        while (node != null) {
             if (node.getKey().equals(key)) {
                 node.setValue(val);
                 return;
             }
             node = node.getNext();
         }
+        if (array[index] != null) {
+            numCollisions++;
+        }
         array[index] = new Node(key, val, array[index]);
-        numCollisions++;
         size++;
     }
 
+    @Override
     public V get(K key) {
         int index = indexOf(key);
         Node<K, V> node = array[index];
@@ -66,29 +63,41 @@ public class HashTable<K, V> {
         return null;
     }
 
+    @Override
     public void delete(K key) {
         int index = indexOf(key);
-        Node<K, V> node = array[index];
-        while (node != null) {
-            if (node.getKey().equals(key)) {
-                if (node.getNext() == null) {
-                    array[index] = null;
-                } else {
-                    array[index].setKey(node.getNext().getKey());
-                    array[index].setValue(node.getNext().getValue());
-                    array[index].setNext(node.getNext().getNext());
-                }
-                return;
-            }
-            node = node.getNext();
+        if (array[index].getKey().equals(key)) {
+            array[index] = null;
+        } else {
+            delete(key, array[index], array[index].getNext());
         }
+        size--;
     }
 
+    public void delete(K key, Node p, Node node) {
+        if (node == null) {
+            return;
+        }
+        if (node.getKey().equals(key)) {
+            p.setNext(node.getNext());
+        }
+        delete(key, node, node.getNext());
+    }
+
+    @Override
+    public void clear() {
+        array = new Node[tableSize];
+        size = 0;
+        numCollisions = 0;
+    }
+
+    @Override
     public void resize() {
-        numResizes++;
         Node[] oldArray = array;
-        array = new Node[tableSize * 2];
         tableSize = tableSize * 2;
+        numResizes++;
+        clear();
+
         for (int i = 0; i < oldArray.length; i++) {
             Node<K, V> node = oldArray[i];
             while (node != null) {
@@ -98,110 +107,89 @@ public class HashTable<K, V> {
         }
     }
 
+    @Override
     public int getSize() {
         return size;
     }
 
+    @Override
+    public int getTableSize() {
+        return tableSize;
+    }
+
+    @Override
     public int getNumCollisions() {
         return numCollisions;
     }
 
+    @Override
     public int getNumResizes() {
         return numResizes;
     }
 
-    private void CreateHashingFunction() {
-
-        Random r = new Random();
-        int a, b, p;
-        a = r.nextInt();
-        b = r.nextInt();
-        p = nextPrime(BIGNUMBER);
-
-        foh = (x) -> Math.abs(((a * x.hashCode() + b * x.hashCode()) * p) % tableSize);
-    }
-
+    @Override
     public void print() {
         for (int i = 0; i < array.length; i++) {
-
+            System.out.print(i + " -> ");
             if (array[i] != null) {
-                System.out.print(i + " -> " + array[i]);
-                print(array[i].getNext());
-            } else {
-                System.out.print(i + " -> x");
+                Node node = array[i];
+                while (node != null) {
+                    System.out.print(" " + node);
+                    node = node.getNext();
+                }
             }
             System.out.println();
         }
     }
 
-    public void print(Node e) {
-        if (e == null) {
-            return;
-        }
-        System.out.print(" - " + e);
-    }
-
-    /* helpers */
-    private static int nextPrime(int a) {
-        while (!isPrime(a)) {
-            a++;
-        }
-        return a;
-    }
-
-    private static boolean isPrime(int a) {
-        if (a == 2) {
-            return true;
-        } else if (a % 2 == 0) {
-            return false;
-        } else {
-            int i = 3, end = (int) Math.sqrt(a + 0.0);
-            while (a % i != 0 && i <= end) {
-                i = i + 2;
+    @Override
+    public void printGraph() {
+        System.out.println("----------- Print Graph-----------");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(i + " | ");
+            if (array[i] != null) {
+                Node node = array[i];
+                while (node != null) {
+                    System.out.print("#");
+                    node = node.getNext();
+                }
             }
-            return i > end;
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        Function<String, Integer> modulo = x -> Math.abs(x.hashCode()) % INITIAL_TABLE_SIZE;
 
-         /*HashTable<String, String> ht = new HashTable<>();
-         ht.put("banana", "yellow");
-         ht.put("apple", "green");
-         ht.put("android", "green");
-         ht.put("cat", "white");
-         ht.put("body", "black");
+        HashTable<String, String> ht = new HashTable<>();
+        ht.put("banana", "yellow");
+        ht.put("apple", "green");
+        ht.put("android", "green");
+        ht.put("cat", "white");
+        ht.put("body", "black");
+        ht.put("class", "red");
+        ht.put("jesy", "pinkman");
+        ht.put("walter", "white");
+        ht.put("arys", "startk");
+        ht.put("dextr", "red");
 
-         System.out.println("----------- Print -----------");
-         ht.print();
+        System.out.println("----------- Print -----------");
+        ht.print();
 
-         System.out.println("----------- Get -----------");
-         String deletedKey = "apple";
-         System.out.println(deletedKey + " : i -> " + ht.indexOf(deletedKey) + " -> " + ht.get(deletedKey));
+        System.out.println("----------- Get -----------");
+        String deletedKey = "apple";
+        System.out.println(deletedKey + " : i -> " + ht.indexOf(deletedKey) + " -> " + ht.get(deletedKey));
 
-         System.out.println("----------- Delete -----------");
-         ht.delete(deletedKey);
-         System.out.println("----------- Print -----------");
-         ht.print();
-         */
-        HashTable<String, String> table = new HashTable<>(modulo);
+        System.out.println("----------- Delete " + deletedKey +" -----------");
+        ht.delete(deletedKey);
 
+        ht.printGraph();
+        ht.printStatus();
+        
+        // Testing
+        HashTable<String, String> table = new HashTable<>();
         for (int i = 0; i < 100000; i++) {
-            table.put(randomString(), randomString());
+            table.put(HashTableInterface.randomString(), HashTableInterface.randomString());
         }
-
-        System.out.println(table.getNumCollisions());
-        System.out.println(table.getNumResizes());
-    }
-
-    public static String randomString() {
-        Random r = new Random(); // perhaps make it a class variable so you don't make a new one every time
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-            char c = (char) (r.nextInt((int) (Character.MAX_VALUE)));
-            sb.append(c);
-        }
-        return sb.toString();
+        table.printStatus();
     }
 }

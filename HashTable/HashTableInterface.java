a28c2ba37bacdbde61c668414d458f45cb1d1485
ben med <benaich.med@gmail.com>
2015-01-03 20:33:23 +0000

    Adding comments

--- HashTable/HashTableInterface.java
+++ HashTable/HashTableInterface.java
@@ -10,34 +10,52 @@ public interface HashTableInterface<K, V> {
     public static final int BIGNUMBER = 9161;
     public static final Random r = new Random();
     public static final String BLACK_CIRCLE = "\u25CF";
-    
+
+    /* get the current load factor */
     default float loadFactor() {
         return getSize() / (float) getTableSize();
     }
 
+    /* get the position of key */
     int indexOf(K key);
 
+    /* update or insert a new item to the hashtable */
     void put(K key, V val);
 
+    /* get the value of the key*/
     V get(K key);
 
+    /* remove key */
     void delete(K key);
     
+    /* check whether the specified key exist in the hashtable */
+    boolean contains(K key);
+
+    /* remove all data from the hashtable */
     void clear();
 
+    /* Double the size of the hashtable */
     void resize();
 
+    /* get the number of elements in the table */
     int getSize();
+
+    /* get the table size */
     int getTableSize();
 
+    /* get the number of collisions */
     int getNumCollisions();
 
+    /* get the number of resizes */
     int getNumResizes();
 
+    /* print the table */
     void print();
-    
+
+    /* print the table like a "graph" */
     void printGraph();
-    
+
+    /* print the table curent status */
     default void printStatus() {
         System.out.println("------------- Table Status -------------------");
         System.out.println("Table size : " + getTableSize());
@@ -45,7 +63,8 @@ public interface HashTableInterface<K, V> {
         System.out.println("Numbre of collisions : " + getNumCollisions());
         System.out.println("Numbre of resises : " + getNumResizes());
     }
-    
+
+    /* return a random universal hashing function */
     default Function<K, Integer> getHashingFunction() {
         int a, b, p;
         a = r.nextInt();
@@ -54,7 +73,8 @@ public interface HashTableInterface<K, V> {
 
         return (x) -> Math.abs(((a * x.hashCode() + b * x.hashCode()) * p) % getTableSize());
     }
-    
+
+    /* get the next prime number after a*/
     static int nextPrime(int a) {
         while (!isPrime(a)) {
             a++;
@@ -62,6 +82,7 @@ public interface HashTableInterface<K, V> {
         return a;
     }
 
+    /* check if a number is prime */
     static boolean isPrime(int a) {
         if (a == 2) {
             return true;
@@ -75,6 +96,8 @@ public interface HashTableInterface<K, V> {
             return i > end;
         }
     }
+
+    /* return a random string (for testing) */
     static String randomString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 10; i++) {
